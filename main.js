ff3700c60acf222833e7737771d1abb3ef6c9243
Ignacio Silva <ignaciosilvadeleon@gmail.com>
2022-12-28 17:13:48 -0300

    feat: L4 done

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
+            inventory: 10,
+            onSale: true,
         }
     }
 })
